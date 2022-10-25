1c7dc8cab1e3b5795fb2c7777586f2aae04ec83f
agustin200208 <agusbarreto6@gmail.com>
2022-10-25 19:22:17 -0300

    probandoPat

---
+++ 1ER_SQL/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(85, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Legajo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(85, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Nombre"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(85, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Apellido"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(192, 29)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 23)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(192, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(152, 23)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(192, 103)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(152, 23)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(436, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(91, 33)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Alta"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(436, 72)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(91, 33)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Baja"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(553, 29)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(91, 33)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Modificar"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(553, 72)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(91, 33)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Consultar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(63, 149)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 25
+        Me.DataGridView1.Size = New System.Drawing.Size(656, 207)
+        Me.DataGridView1.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ 1ER_SQL/Form1.vb
@@ -0,0 +1,98 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim MiStrindDeConexion As String
+    Dim MiConexion As SqlConnection
+    Dim MiDataSet As DataSet
+    Dim MiSqlComand As SqlCommand
+    Dim MiSqlAdapter As SqlDataAdapter
+    Dim MiDataTable As DataTable
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.DataGridView1.Columns.Add("0", "Legajo")
+        Me.DataGridView1.Columns.Add("1", "Nombre")
+        Me.DataGridView1.Columns.Add("2", "Apellido")
+    End Sub
+
+    Sub ConsultarGrilla()
+        MiStrindDeConexion = "Data source=AGUSTIN\SQLEXPRESS;Initial Catalog=Alumnos;Integrated Security = True"
+        Dim MiConsulta As String = ("Select * from DATOS")
+        MiConexion = New SqlConnection(MiStrindDeConexion)
+        MiSqlComand = New SqlCommand(MiConsulta)
+        MiSqlComand.Connection = MiConexion
+        MiConexion.Open()
+
+        MiDataSet = New DataSet
+        MiDataTable = New DataTable
+        MiSqlAdapter = New SqlDataAdapter
+
+        MiSqlAdapter.SelectCommand = MiSqlComand
+        MiSqlAdapter.Fill(MiDataSet)
+
+        MiDataTable = MiDataSet.Tables(0)
+
+        MiConexion.Close()
+
+        Me.DataGridView1.Rows.Clear()
+        Dim fila As Integer = 0
+
+        For Each miFila As DataRow In MiDataTable.Rows
+            Me.DataGridView1.Rows.Add()
+
+            Me.DataGridView1.Rows(fila).Cells(0).Value = miFila.Item(0)
+            Me.DataGridView1.Rows(fila).Cells(1).Value = miFila.Item(1)
+            Me.DataGridView1.Rows(fila).Cells(2).Value = miFila.Item(2)
+            fila += 1
+        Next
+    End Sub
+
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ConsultarGrilla()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MiStrindDeConexion = "Data source=AGUSTIN\SQLEXPRESS;Initial Catalog=Alumnos;Integrated Security = True"
+
+        Dim MiConsulta As String = ("INSERT INTO DATOS VALUES('" & TextBox1.Text & "','" & TextBox2.Text & " ','" & TextBox3.Text & " ')")
+        MiConexion = New SqlConnection(MiStrindDeConexion)
+        MiSqlComand = New SqlCommand(MiConsulta)
+        MiSqlComand.Connection = MiConexion
+
+        MiConexion.Open()
+        MiSqlComand.ExecuteNonQuery()
+        MiConexion.Close()
+
+        MsgBox("El estudiante ah sido de alta")
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        ConsultarGrilla()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MiStrindDeConexion = "Data source=AGUSTIN\SQLEXPRESS;Initial Catalog=Alumnos;Integrated Security = True"
+
+        Dim MiConsulta As String = ("DELETE FROM DATOS WHERE IDalumno= '" & TextBox1.Text & "'")
+        MiConexion = New SqlConnection(MiStrindDeConexion)
+        MiSqlComand = New SqlCommand(MiConsulta)
+        MiSqlComand.Connection = MiConexion
+
+        MiConexion.Open()
+        MiSqlComand.ExecuteNonQuery()
+        MiConexion.Close()
+
+        MsgBox("El estudiante ah sido de baja")
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        ConsultarGrilla()
+    End Sub
+
+    Private Sub DataGridView1_CellValidated(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellValidated
+        TextBox1.Text = DataGridView1.SelectedRows(0).Cells(0).Value()
+        TextBox2.Text = DataGridView1.SelectedRows(0).Cells(1).Value()
+        TextBox3.Text = DataGridView1.SelectedRows(0).Cells(2).Value()
+    End Sub
+End Class
